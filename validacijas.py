7aae95f37586902c06b701207cd3f84a87aeeadf
Jēkabs <jekabs.balodis@gmail.com>
2021-09-29 11:20:32 +0300

    Reverting changes

--- validacijas.py
+++ validacijas.py
@@ -84,12 +84,11 @@ try:
             eTalons = ride[7]
             laiks = ride[8]
             # Inserting selected data from txt file to sqlite database
-            
-        try:
+
             cur.execute('INSERT OR IGNORE INTO Parks (parks) VALUES ( ? )', ( parks, ) )
             cur.execute('SELECT id FROM Parks WHERE parks = ? ', (parks, ))
             parks_id = cur.fetchone()[0]
-            
+
             cur.execute('INSERT OR IGNORE INTO Tr_veids (tr_veids) VALUES ( ? )', ( tr_veids, ) )
             cur.execute('SELECT id FROM Tr_veids WHERE tr_veids = ? ', (tr_veids, ))
             tr_veids_id = cur.fetchone()[0]
@@ -120,16 +119,12 @@ try:
             (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
             (id, parks_id, tr_veids_id, borta_nr_id,marsr_nos_id, marsrs_id, virziens_id, eTalons_id, laiks ))
             i = i + 1
-
             if i == 50000:
                 conn.commit()
                 print('commit')
-                # print('sleep for 5 seconds')
-                # time.sleep(5)
+                print('sleep for 5 seconds')
+                time.sleep(5)
                 i = 0
-
-        except:
-            print(file)
 
 except KeyboardInterrupt:
     print('\nstopped by user')
