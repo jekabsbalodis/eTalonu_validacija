aed39c9ec435d6bdd56a904ff1ddd8bc722fabfa
Balodis09 <jekabs.balodis@gmail.com>
2021-11-22 15:49:07 +0200

    Izmaiņas failu saraksta izveidē

---
+++ files.py
@@ -0,0 +1,55 @@
+import sqlite3
+import os
+import time
+import chardet
+
+# Create database for storing information about file encodings
+conn = sqlite3.connect('data/encoding.sqlite')
+cur = conn.cursor()
+
+# Create tables for data
+cur.executescript('''
+CREATE TABLE IF NOT EXISTS Files (
+    name TEXT UNIQUE,
+    encoding_id INTEGER,
+    times_read INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS Encodings (
+    encoding_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    encoding TEXT UNIQUE
+)
+''')
+
+# Writing information to database
+for paths, dirs, files in os.walk('raw_data'):
+    i = 0
+    for file in files:
+        if file.startswith('.'):
+            continue
+        filePath = os.path.join(paths, file)
+        with open(filePath) as openFile:
+            encoding = openFile.encoding
+        cur.execute('SELECT name FROM Files WHERE name = ?', (file,))
+        check = cur.fetchone()
+        if check:
+            # print('File info already stored')
+            continue
+        else:
+            cur.execute(
+                'INSERT OR IGNORE INTO Encodings (encoding) VALUES (?)', (encoding,))
+            cur.execute(
+                'SELECT encoding_id FROM Encodings WHERE encoding = ?', (encoding,))
+            encodingId = cur.fetchone()[0]
+
+            cur.execute('''INSERT OR IGNORE INTO Files
+            (name, encoding_id, times_read) VALUES (?, ?, ?)''', (file, encodingId, 0))
+        i = i + 1
+        if i == 50000:
+            conn.commit()
+            print('commit')
+            print('sleep for 5 seconds')
+            time.sleep(5)
+            i = 0
+conn.commit()
+conn.close()
--- validacijas.py
+++ validacijas.py
@@ -2,13 +2,6 @@ import sqlite3
 import os
 import time
 
-# Prepare to open txt files with data from Rīgas Satiksme
-allfiles = set()
-for paths, dirs, files in os.walk('raw_data'):
-    for file in files:
-        if file.startswith('.') : continue
-        allfiles.add(os.path.join(paths, file)) # Adds all files with their relative location to set allfiles
-
 # Create sqlite database to store the data from txt file
 conn = sqlite3.connect('data/validacijas.sqlite')
 cur = conn.cursor()
@@ -64,15 +57,19 @@ CREATE TABLE IF NOT EXISTS ETalons (
 )
 ''')
 
+# Opening database for file information
+files = sqlite3.connect('data\encoding.sqlite')
+
 # Opening individual files
 try:
     for file in allfiles:
-        fhandle = open(file,encoding='Windows 1257')
+        fhandle = open(file, encoding='Windows 1257')
 
         i = 0
-        for line in fhandle: # Reading the file line for line and selecting necessary data
+        for line in fhandle:  # Reading the file line for line and selecting necessary data
             line = line.rstrip()
-            if line.startswith('Ier_ID') : continue # Skipping the line with "Headers"
+            if line.startswith('Ier_ID'):
+                continue  # Skipping the line with "Headers"
             ride = line.split(',')
             id = ride[0]
             parks = ride[1]
@@ -83,51 +80,61 @@ try:
             virziens = ride[6]
             eTalons = ride[7]
             laiks = ride[8]
+
             # Inserting selected data from txt file to sqlite database
+            cur.execute(
+                'INSERT OR IGNORE INTO Parks (parks) VALUES ( ? )', (parks, ))
+            cur.execute('SELECT id FROM Parks WHERE parks = ? ', (parks, ))
+            parks_id = cur.fetchone()[0]
 
-            try:
-                cur.execute('INSERT OR IGNORE INTO Parks (parks) VALUES ( ? )', ( parks, ) )
-                cur.execute('SELECT id FROM Parks WHERE parks = ? ', (parks, ))
-                parks_id = cur.fetchone()[0]
+            cur.execute(
+                'INSERT OR IGNORE INTO Tr_veids (tr_veids) VALUES ( ? )', (tr_veids, ))
+            cur.execute(
+                'SELECT id FROM Tr_veids WHERE tr_veids = ? ', (tr_veids, ))
+            tr_veids_id = cur.fetchone()[0]
 
-                cur.execute('INSERT OR IGNORE INTO Tr_veids (tr_veids) VALUES ( ? )', ( tr_veids, ) )
-                cur.execute('SELECT id FROM Tr_veids WHERE tr_veids = ? ', (tr_veids, ))
-                tr_veids_id = cur.fetchone()[0]
+            cur.execute(
+                'INSERT OR IGNORE INTO Borta_nr (borta_nr) VALUES ( ? )', (borta_nr, ))
+            cur.execute(
+                'SELECT id FROM Borta_nr WHERE borta_nr = ? ', (borta_nr, ))
+            borta_nr_id = cur.fetchone()[0]
 
-                cur.execute('INSERT OR IGNORE INTO Borta_nr (borta_nr) VALUES ( ? )', ( borta_nr, ) )
-                cur.execute('SELECT id FROM Borta_nr WHERE borta_nr = ? ', (borta_nr, ))
-                borta_nr_id = cur.fetchone()[0]
+            cur.execute(
+                'INSERT OR IGNORE INTO Marsruta_nos (marsruta_nos) VALUES ( ? )', (marsr_nos, ))
+            cur.execute(
+                'SELECT id FROM Marsruta_nos WHERE marsruta_nos = ? ', (marsr_nos, ))
+            marsr_nos_id = cur.fetchone()[0]
 
-                cur.execute('INSERT OR IGNORE INTO Marsruta_nos (marsruta_nos) VALUES ( ? )', ( marsr_nos, ) )
-                cur.execute('SELECT id FROM Marsruta_nos WHERE marsruta_nos = ? ', (marsr_nos, ))
-                marsr_nos_id = cur.fetchone()[0]
+            cur.execute(
+                'INSERT OR IGNORE INTO Marsruts (marsruts) VALUES ( ? )', (marsr, ))
+            cur.execute(
+                'SELECT id FROM Marsruts WHERE marsruts = ? ', (marsr, ))
+            marsrs_id = cur.fetchone()[0]
 
-                cur.execute('INSERT OR IGNORE INTO Marsruts (marsruts) VALUES ( ? )', ( marsr, ) )
-                cur.execute('SELECT id FROM Marsruts WHERE marsruts = ? ', (marsr, ))
-                marsrs_id = cur.fetchone()[0]
+            cur.execute(
+                'INSERT OR IGNORE INTO Virziens (virziens) VALUES ( ? )', (virziens, ))
+            cur.execute(
+                'SELECT id FROM Virziens WHERE virziens = ? ', (virziens, ))
+            virziens_id = cur.fetchone()[0]
 
-                cur.execute('INSERT OR IGNORE INTO Virziens (virziens) VALUES ( ? )', ( virziens, ) )
-                cur.execute('SELECT id FROM Virziens WHERE virziens = ? ', (virziens, ))
-                virziens_id = cur.fetchone()[0]
+            cur.execute(
+                'INSERT OR IGNORE INTO ETalons (eTalons) VALUES ( ? )', (eTalons, ))
+            cur.execute(
+                'SELECT id FROM ETalons WHERE eTalons = ? ', (eTalons, ))
+            eTalons_id = cur.fetchone()[0]
 
-                cur.execute('INSERT OR IGNORE INTO ETalons (eTalons) VALUES ( ? )', ( eTalons, ) )
-                cur.execute('SELECT id FROM ETalons WHERE eTalons = ? ', (eTalons, ))
-                eTalons_id = cur.fetchone()[0]
-
-                cur.execute('''INSERT OR IGNORE INTO Validacijas
-                (id, parks_id, tr_veids_id, borta_nr_id, marsruta_nos_id, marsruts_id, virziens_id, etalons_id, laiks)
-                VALUES
-                (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                (id, parks_id, tr_veids_id, borta_nr_id,marsr_nos_id, marsrs_id, virziens_id, eTalons_id, laiks ))
-                i = i + 1
-                if i == 50000:
-                    conn.commit()
-                    print('commit')
-                    print('sleep for 5 seconds')
-                    time.sleep(5)
-                    i = 0
-            except:
-                print(file)
+            cur.execute('''INSERT OR IGNORE INTO Validacijas
+            (id, parks_id, tr_veids_id, borta_nr_id, marsruta_nos_id, marsruts_id, virziens_id, etalons_id, laiks)
+            VALUES
+            (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                        (id, parks_id, tr_veids_id, borta_nr_id, marsr_nos_id, marsrs_id, virziens_id, eTalons_id, laiks))
+            i = i + 1
+            if i == 50000:
+                conn.commit()
+                print('commit')
+                print('sleep for 5 seconds')
+                time.sleep(5)
+                i = 0
 
 except KeyboardInterrupt:
     print('\nstopped by user')
