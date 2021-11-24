2dc11febb2de0013fc617eb9d6b779553f43e886
Jekabs Balodis <jekabs.balodis@gmail.com>
2021-11-24 17:01:30 +0200

    Changes to file reading

--- files.py
+++ files.py
@@ -23,28 +23,28 @@ CREATE TABLE IF NOT EXISTS Encodings (
 )
 ''')
 
-# Writing information to database
 i = 0
+# Writing information to database
 for paths, dirs, files in os.walk('raw_data'):
     for file in files:
         if file.startswith('.'):
             continue
-        filePath = os.path.join(paths, file)
-        detector.reset()
-        with open(filePath, 'rb') as openFile:
-            for line in openFile:
-                detector.feed(line)
-                if detector.done:
-                    break
-            detector.close()
-            encoding = detector.result['encoding']
-        print(encoding)
         cur.execute('SELECT name FROM Files WHERE name = ?', (file,))
         check = cur.fetchone()
         if check:
-            # print('File info already stored')
+            print('File info already stored')
             continue
         else:
+            filePath = os.path.join(paths, file)
+            detector.reset()
+            with open(filePath, 'rb') as openFile:
+                for line in openFile:
+                    detector.feed(line)
+                    if detector.done:
+                        break
+                detector.close()
+                encoding = detector.result['encoding']
+            print(encoding)
             cur.execute(
                 'INSERT OR IGNORE INTO Encodings (encoding) VALUES (?)', (encoding,))
             cur.execute(
@@ -54,11 +54,8 @@ for paths, dirs, files in os.walk('raw_data'):
             cur.execute('''INSERT OR IGNORE INTO Files
             (name, encoding_id, times_read) VALUES (?, ?, ?)''', (file, encodingId, 0))
         i = i + 1
-        if i == 50000:
-            conn.commit()
-            print('commit')
-            print('sleep for 5 seconds')
-            time.sleep(5)
-            i = 0
+        conn.commit()
+        print('commit')
+        print('Read file ', i)
 conn.commit()
 conn.close()
