fc9d04b61613306696990a13b6652118c27d3ad6
jekabsbalodis <signalingpigeon@duck.com>
2024-07-11 13:56:14 +0000

    Adding AutoField and DateTimeField

--- app/models.py
+++ app/models.py
@@ -9,37 +9,37 @@ class BaseModel(pw.Model):
 
 
 class Parks(BaseModel):
-    id = pw.IntegerField(unique=True, primary_key=True)
+    id = pw.AutoField()
     parks = pw.CharField(max_length=10, unique=True)
 
 
 class Transports(BaseModel):
-    id = pw.IntegerField(unique=True, primary_key=True)
+    id = pw.AutoField()
     transports = pw.CharField(max_length=32, unique=True)
 
 
 class GarNr(BaseModel):
-    id = pw.IntegerField(unique=True, primary_key=True)
+    id = pw.AutoField()
     gar = pw.IntegerField(unique=True)
 
 
 class Marsruts(BaseModel):
-    id = pw.IntegerField(unique=True, primary_key=True)
+    id = pw.AutoField()
     mars_nos = pw.CharField()
     marsruts = pw.CharField(max_length=10, unique=True)
 
 
 class Talons(BaseModel):
-    id = pw.IntegerField(unique=True, primary_key=True)
+    id = pw.AutoField()
     talons = pw.IntegerField(unique=True)
 
 
 class Validacijas(BaseModel):
-    id = pw.IntegerField(unique=True, primary_key=True)
-    parks_id = pw.ForeignKeyField(Parks, backref='validacijas')
-    transp_id = pw.ForeignKeyField(Transports, backref='validacijas')
-    gar_nr_id = pw.ForeignKeyField(GarNr, backref='validacijas')
-    marsruts_id = pw.ForeignKeyField(Marsruts, backref='validacijas')
+    id = pw.AutoField()
+    parks_id = pw.ForeignKeyField(Parks)
+    transp_id = pw.ForeignKeyField(Transports)
+    gar_nr_id = pw.ForeignKeyField(GarNr)
+    marsruts_id = pw.ForeignKeyField(Marsruts)
     virziens = pw.BooleanField()
-    talona_id = pw.ForeignKeyField(Talons, backref='validacijas')
-    laiks = pw.CharField(max_length=32)
+    talona_id = pw.ForeignKeyField(Talons)
+    laiks = pw.DateTimeField()
