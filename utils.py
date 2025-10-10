bd84f093d444520c101bc04295b1ab229bef7798
jekabsbalodis <signalingpigeon@duck.com>
2025-10-10 19:26:42 +0300

    Adding utility functions

---
+++ utils.py
@@ -0,0 +1,27 @@
+"""
+Utility functions.
+"""
+
+from calendar import monthrange
+from datetime import date
+
+from babel import Locale
+from babel.numbers import format_compact_decimal
+
+_locale = Locale('lv')
+
+
+def format_number(value: int, **kwargs) -> str:
+    """
+    Format number with Latvian locale.
+    """
+    return format_compact_decimal(value, locale=_locale, **kwargs)
+
+
+def last_day_of_month(start_day: date) -> date:
+    """
+    Return the last day of the start_day month.
+    """
+    return date(
+        start_day.year, start_day.month, monthrange(start_day.year, start_day.month)[1]
+    )
