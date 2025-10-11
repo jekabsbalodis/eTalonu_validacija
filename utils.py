31e38e93b731ed983b486a044f25a70d973515bb
clear9550 <clear9550@noreply.codeberg.org>
2025-10-11 13:34:16 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from datetime import date
 
 from babel import Locale
 from babel.numbers import format_compact_decimal
+from babel.dates import formar_date
 
 _locale = Locale('lv')
 
@@ -25,3 +26,9 @@ def last_day_of_month(start_day: date) -> date:
     return date(
         start_day.year, start_day.month, monthrange(start_day.year, start_day.month)[1]
     )
+
+def format_date(date: date) -> str:
+    """
+    Format date to show month and year with Latvian locale
+    """
+    return format_date(date, format='MMMM YYYY', locale = _locale)
