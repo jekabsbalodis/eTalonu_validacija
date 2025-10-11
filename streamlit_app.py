940bee64be0ce0eec636601828410fbf351ce8d4
clear9550 <clear9550@noreply.codeberg.org>
2025-10-11 13:03:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from database import db
 from state_manager import MetricsKeys, StateKeys, init_state
 from utils import format_number
 
-# locale.setlocale(locale.LC_ALL, 'lv_LV.utf8')
+locale.setlocale(locale.LC_TIME, 'lv_LV.utf8')
 
 init_state(db, st.session_state)
 
